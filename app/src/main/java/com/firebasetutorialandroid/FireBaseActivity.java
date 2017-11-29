e3af45de829929b92f8c41e8aefd88b024ab1d34
tikson <tiksontom@gmail.com>
2017-11-29 11:58:31 +0530

    Initial commit

---
+++ app/src/main/java/com/firebasetutorialandroid/FireBaseActivity.java
@@ -0,0 +1,13 @@
+package com.firebasetutorialandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FireBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fire_base);
+    }
+}
